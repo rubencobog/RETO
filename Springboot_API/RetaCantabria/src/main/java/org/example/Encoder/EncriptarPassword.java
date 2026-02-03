8a6e863369312b9d905fa9e10d30ca29d4cc8436
Saul GG <152623929+SaulGarciaaaa@users.noreply.github.com>
2026-02-03 18:10:03 +0100

    Cambios

--- Springboot_API/RetaCantabria/src/main/java/org/example/Encoder/EncriptarPassword.java
+++ Springboot_API/RetaCantabria/src/main/java/org/example/Encoder/EncriptarPassword.java
@@ -1,14 +1,6 @@
 package org.example.Encoder;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class EncriptarPassword {
-    private PasswordEncoder encoder;
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
