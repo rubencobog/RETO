94b7e436008853b600fc51bc552a5c1ac4026380
Saul GG <152623929+SaulGarciaaaa@users.noreply.github.com>
2026-02-04 18:19:32 +0100

    Cambios API de relacion de Puntos de Peligro

--- Springboot_API/RetaCantabria/src/main/java/org/example/Conexion/PuntoPeligroController.java
+++ Springboot_API/RetaCantabria/src/main/java/org/example/Conexion/PuntoPeligroController.java
@@ -47,4 +47,9 @@ public class PuntoPeligroController {
     public void delete(@PathVariable Long id) {
         puntoPeligroService.eliminar(id);
     }
+
+    @GetMapping("/buscaPP")
+    public List<PuntoPeligro> buscarppruta(@RequestParam Long id){
+        return puntoPeligroService.puntospeligroRuta(id);
+    }
 }
--- Springboot_API/RetaCantabria/src/main/java/org/example/Logica/PuntoPeligroRepository.java
+++ Springboot_API/RetaCantabria/src/main/java/org/example/Logica/PuntoPeligroRepository.java
@@ -3,6 +3,12 @@ package org.example.Logica;
 
 import org.example.Entidades.PuntoPeligro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PuntoPeligroRepository extends JpaRepository<PuntoPeligro, Long> {
+@Query("SELECT pp FROM PuntoPeligro pp  WHERE pp.puntoRuta.idPuntoRuta = :idPP")
+ List<PuntoPeligro> getPuntoPeligro(@Param("idRuta") long  id);
 }
