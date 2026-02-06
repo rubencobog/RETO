f5fca42e16351e290549e06a842d1e71ff4c2d0a
Fran898 <147599265+Fran898@users.noreply.github.com>
2026-02-06 17:42:28 +0100

    Login API

--- AplicacionMovil/app/build.gradle.kts
+++ AplicacionMovil/app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- AplicacionMovil/app/src/main/java/com/example/retodam2rutas/views/LoginViewModel.kt
+++ AplicacionMovil/app/src/main/java/com/example/retodam2rutas/views/LoginViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.retodam2rutas.data.database.AppDatabase
+import com.example.retodam2rutas.data.service.UsuarioServiceFactory
 import com.example.retodam2rutas.model.UsuarioModel
 
 class LoginViewModel(
@@ -12,7 +13,12 @@ class LoginViewModel(
 ) : ViewModel() {
     private val _usuario = MutableLiveData<UsuarioModel>()
     val usuario: LiveData<UsuarioModel> = _usuario
+    val usuarioService = UsuarioServiceFactory.makeUsuarioService()
 
+    fun getLoginUsuario(email: String, password: String){
+        val usuario = usuarioService.getLogin(email, password)
+
+    }
 
 
 }
