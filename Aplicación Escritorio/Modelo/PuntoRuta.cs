8becebbf2550e3033bbee8c708210346ff424ef0
Saul GG <152623929+SaulGarciaaaa@users.noreply.github.com>
2026-02-10 15:16:17 +0100

    Cambios Ficheros

--- Aplicación Escritorio/Modelo/PuntoRuta.cs
+++ Aplicación Escritorio/Modelo/PuntoRuta.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Modelo
@@ -13,6 +14,7 @@ namespace Modelo
         public double latitud { get; set; }
         public int elevacion { get; set; }
         public DateTime timestamp { get; set; }
+        [JsonIgnore]
         public Ruta ruta { get; set; }
 
     }
--- Aplicación Escritorio/RetaCantabria/Fichas.Designer.cs
+++ Aplicación Escritorio/RetaCantabria/Fichas.Designer.cs
@@ -28,11 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             cbSeguridad = new CheckBox();
             cbUsuario = new CheckBox();
             cbOrganizacion = new CheckBox();
             button1 = new Button();
             label1 = new Label();
+            imageList1 = new ImageList(components);
+            pb_Seguridad = new PictureBox();
+            pb_Usuario = new PictureBox();
+            pb_Organizacion = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pb_Seguridad).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb_Usuario).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb_Organizacion).BeginInit();
             SuspendLayout();
             // 
             // cbSeguridad
@@ -48,7 +56,7 @@
             // cbUsuario
             // 
             cbUsuario.AutoSize = true;
-            cbUsuario.Location = new Point(41, 191);
+            cbUsuario.Location = new Point(41, 169);
             cbUsuario.Name = "cbUsuario";
             cbUsuario.Size = new Size(113, 19);
             cbUsuario.TabIndex = 1;
@@ -58,13 +66,12 @@
             // cbOrganizacion
             // 
             cbOrganizacion.AutoSize = true;
-            cbOrganizacion.Location = new Point(41, 334);
+            cbOrganizacion.Location = new Point(41, 301);
             cbOrganizacion.Name = "cbOrganizacion";
             cbOrganizacion.Size = new Size(143, 19);
             cbOrganizacion.TabIndex = 2;
             cbOrganizacion.Text = "Ficha de Organización";
             cbOrganizacion.UseVisualStyleBackColor = true;
-            cbOrganizacion.CheckedChanged += checkBox3_CheckedChanged;
             // 
             // button1
             // 
@@ -85,11 +92,44 @@
             label1.TabIndex = 4;
             label1.Text = "Fichas Informativas";
             // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // pb_Seguridad
+            // 
+            pb_Seguridad.Location = new Point(559, 43);
+            pb_Seguridad.Name = "pb_Seguridad";
+            pb_Seguridad.Size = new Size(184, 115);
+            pb_Seguridad.TabIndex = 5;
+            pb_Seguridad.TabStop = false;
+            // 
+            // pb_Usuario
+            // 
+            pb_Usuario.Location = new Point(559, 169);
+            pb_Usuario.Name = "pb_Usuario";
+            pb_Usuario.Size = new Size(184, 115);
+            pb_Usuario.TabIndex = 6;
+            pb_Usuario.TabStop = false;
+            // 
+            // pb_Organizacion
+            // 
+            pb_Organizacion.Location = new Point(559, 301);
+            pb_Organizacion.Name = "pb_Organizacion";
+            pb_Organizacion.Size = new Size(184, 115);
+            pb_Organizacion.TabIndex = 7;
+            pb_Organizacion.TabStop = false;
+            // 
             // Fichas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pb_Organizacion);
+            Controls.Add(pb_Usuario);
+            Controls.Add(pb_Seguridad);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(cbOrganizacion);
@@ -97,6 +137,9 @@
             Controls.Add(cbSeguridad);
             Name = "Fichas";
             Text = "Fichas";
+            ((System.ComponentModel.ISupportInitialize)pb_Seguridad).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb_Usuario).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb_Organizacion).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -108,5 +151,9 @@
         private CheckBox cbOrganizacion;
         private Button button1;
         private Label label1;
+        private ImageList imageList1;
+        private PictureBox pb_Seguridad;
+        private PictureBox pb_Usuario;
+        private PictureBox pb_Organizacion;
     }
 }
